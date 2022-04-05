f1b723b4c308d340053257c33de265a93d58a949
Nicolas <nicolasdemaria4@gmail.com>
2022-04-05 16:40:05 -0300

    +1

---
+++ Unit02/artGallery.py
@@ -0,0 +1,9 @@
+# 11. Galería de arte
+# Una galería de arte desea preparar un catálogo de sus cuadros más famosos.
+
+# Se realiza una prueba con tres cuadros y por cada uno se ingresa el año en que fue creado. El programa deberá:
+
+# Verificar si todos los cuadros son anteriores al siglo XX(El siglo XX es el siglo pasado. Se inició en el año 1901 y terminó en el año 2000).
+# Determinar si alguna de las obras fue creada en un año que se ingresa por teclado.
+# Informar la diferencia en años entre la obra más nueva y la más antigua.
+
