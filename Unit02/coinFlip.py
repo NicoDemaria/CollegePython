8a893f4a3ff66111d944e0060b2e24a67def354a
Nicolas <nicolasdemaria4@gmail.com>
2022-04-05 10:12:21 -0300

    Create coinFlip.py

---
+++ Unit02/coinFlip.py
@@ -0,0 +1,2 @@
+# 7. Tirada de moneda
+# Programar una tirada de una moneda (opciones: cara o cruz) aleatoriamente. Permitir que un jugador apueste a cara o cruz y luego informar si acertó o no con su apuesta.
