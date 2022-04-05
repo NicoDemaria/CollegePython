5e440c4c3642b4ad885e87e2b3176fcb24704b61
Nicolas <nicolasdemaria4@gmail.com>
2022-04-05 10:02:22 -0300

    +1

--- Unit02/palabras09.py
+++ Unit02/palabras09.py
@@ -3,3 +3,12 @@
 
 # Determinar la cantidad de letras que tiene  la palabra.
 # Mostrar un mensaje que informe si la palabra termina en vocal.
+
+
+p1 = str(input('Ingrese una palabra: '))
+
+
+print('La palabra tiene la siguiente cantidad de caracteres: ', len(p1), '\n ')
+
+if p1[-1] == 'a' or p1[-1] == 'e' or p1[-1] == 'i' or p1[-1] == 'o' or p1[-1] == 'u':
+    print('La palabra termina con vocal', p1[-1])
