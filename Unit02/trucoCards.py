8b46eb0baf8207474624720372b769c8c3c6f969
Nicolas <nicolasdemaria4@gmail.com>
2022-04-05 18:22:06 -0300

    Update trucoCards.py

--- Unit02/trucoCards.py
+++ Unit02/trucoCards.py
@@ -4,7 +4,7 @@
 # Verificar si las tres cartas son del mismo palo. Si es así, mostrar la suma de las dos mayores. En caso contrario, informarlo.
 
 import random
-manoTruco = random.randint(1,12)
+manoTruco = random.randint(1,1)
 manoTruco2 = random.randint(1,12)
 manoTruco3 = random.randint(1,12)
 
@@ -17,6 +17,20 @@ manoPalo3  = random.choices(palos)
 mano1 = (manoTruco,manoPalo1)
 mano2 = (manoTruco2,manoPalo2)
 mano3 = (manoTruco3,manoPalo3)
-
 print(mano1,mano2,mano3)
+
+if mano1 == (1, ['Espada']) or mano2 == (1, ['Espada']) or mano3 == (1, ['Espada']):
+    print('Te toco el macho')
+else:
+    print('No te toco el macho')
+
+
+
+maxCard = (max(manoTruco,manoTruco2,manoPalo3))
+
+if manoPalo1 == manoPalo2 == manoPalo3:
+    print(maxCard)
+
+
+
 
