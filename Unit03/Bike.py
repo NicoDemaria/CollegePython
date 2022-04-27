b483ab3a27b5c2995cc5c79e2e2d69f57171b9c6
Nicolas <nicolasdemaria4@gmail.com>
2022-04-27 19:30:55 -0300

    Update Bike.py

--- Unit03/Bike.py
+++ Unit03/Bike.py
@@ -1,27 +1,39 @@
 # 1. Ciclistas
 # La final de una carrera de ciclistas tiene n competidores (n se ingresa por teclado).
 
-# Desarrollar un programa que permita cargar, por cada competidor, nombre y tiempo de carrera. Luego se pide:
+# A un programa que permita cargar, por cada competidor, nombre y tiempo de carrera. Luego se pide:
 
 # a) Determinar y mostrar el nombre del ganador de la carrera.
 
-# b) Ingresar por teclado el tiempo record registrado para dicha carrera. Determinar si el tiempo del ganador es menor al tiempo record, mostrar un mensaje.
+# Ab) Ingresar por teclado el tiempo record registrado para dicha carrera. Determinar si el tiempo del ganador es menor al tiempo record, mostrar un mensaje.
 
-# c) Calcular y mostrar el tiempo promedio entre todos los ciclistas.
+# Ac) Calcular y mostrar el tiempo promedio entre todos los ciclistas.
 
 
-n = input("Ingrese la cantidad de competidores: ")
-contador = 0
-for i in range(int(n)):
-    contador += int(tiempo)
-    promedio = contador / int(n)
+cant = int(input('Ingrese la cantidad de competidores: '))
+contadorProm = 0
+for i in range(1, cant+1):
+    name = str(input('Ingrese el nombre de el competidor: '))
+    time = int(input('Ingrese el tiempo que realizo en minutos: '))
+    contadorProm += time
+    print('El nombre de el competidor es:',name,'y su tiempo es:',time)
+    if i == 1:
+        mayor = time
+    else:
+        if time > mayor :
+            mayor = time
+print('=========================================================')
+promedio = contadorProm/cant
+print('El promedio de tiempo entre los ciclistas es: ', promedio)
+print('=========================================================')
 
-    print("Competidor ", i + 1)
-    nombre = input("Ingrese el nombre del competidor: ")
-    tiempo = input("Ingrese el tiempo del competidor(minutos): ")
-    print("Nombre: ", nombre, " Tiempo: ", tiempo)
+print('=========================================================')
+print('El ganador es: ',mayor, name)
+print('=========================================================')
 
-tiempoRecord = input("Ingrese el tiempo record: ")
-if tiempo > tiempoRecord:
-    print("El tiempo del competidor es mayor al tiempo record",
-          tiempo, ">", tiempoRecord)
+timeRecord = int(input('Ingrese tiempo record: '))
+
+if mayor < timeRecord:
+    print('El tiempo record fue superado,', mayor)
+else:
+    print('El tiempo record no fue superado.')
