b1ca77f1902ef2a63fbb625980aed50db17e60fd
Nicolas <nicolasdemaria4@gmail.com>
2022-07-21 16:11:20 -0300

    Create 08_funciones.py

---
+++ Unit04/08_funciones.py
@@ -0,0 +1,5 @@
+'''Programa que pide dos numeros al usuario y realiza difrentes operaciones matematicas'''
+
+
+n1 = int(input('Ingrese el primer numero para realizar las operaciones: '))
+n2 = int(input('Ingrese el segundo numero para realizar las operaciones'))
