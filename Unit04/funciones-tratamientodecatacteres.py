dcae8c4e95228e675a45c51dd007e5eaed06c34d
Nicolas <nicolasdemaria4@gmail.com>
2022-07-25 21:12:43 -0300

    Update funciones-tratamientodecatacteres.py

--- Unit04/funciones-tratamientodecatacteres.py
+++ Unit04/funciones-tratamientodecatacteres.py
@@ -1,30 +1,86 @@
-# Es una vocal?
+# módulo con funciones para procesamiento de caracteres
 
-def esVocal(car):
-    if car in 'aeiouAEIOU':
-        return True
-    return False
+def es_digito(car):
+    res = False
 
+    if car in "0123456789":
+        res = True
 
-# Es un digito?
-
-def esDigito(car):
-    if car in '0123456789':
-        return True
-    return False
-
-# Es blanco o un punto?
+    return res
 
 
-def esTerminador(car):
-    if car in '.':
-        return True
-    return False
+def es_vocal(car):
+    res = False
 
-# Es una consonante?
+    if car.lower() in "aeiouáéíóúü":
+        res = True
+
+    return res
 
 
-def esConsonante(car):
-    if car in 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ':
-        return True
-    return False
+def es_consonante(car):
+    res = False
+
+    if car.lower() in "bcdfghjklmnñpqrstvwxyz":
+        res = True
+
+    return res
+
+
+def es_letra(car):
+    res = False
+
+    if es_vocal(car) or es_consonante(car):
+        res = True
+
+    return res
+
+
+def es_mayuscula(car):
+    res = False
+
+    if car in "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ":
+        res = True
+
+    return res
+
+
+def prueba():
+    print(es_vocal("A"))
+    print(es_vocal("g"))
+    print(es_consonante("T"))
+    print(es_consonante("e"))
+    print(es_letra("Y"))
+    print(es_letra("-"))
+    print(es_mayuscula("A"))
+    print(es_mayuscula("a"))
+
+
+# script principal
+if __name__ == '__main__':
+    prueba()
+
+
+# módulo con funciones para procesamiento de caracteres
+def es_digito(car):
+    return car in "0123456789"
+
+
+def es_vocal(car):
+    return car.lower() in "aeiouáéíóúü"
+
+
+def es_consonante(car):
+    return car.lower() in "bcdfghjklmnñpqrstvwxyz"
+
+
+def es_letra(car):
+    return es_vocal(car) or es_consonante(car)
+
+
+def es_mayuscula_v1(car):
+    return car in "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+
+def es_mayuscula_v2(car):
+    return es_letra(car) and car == car.upper()
