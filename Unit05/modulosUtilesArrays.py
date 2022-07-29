c864b89c82c6883c462dece54cf14b8a1c9e0c7f
Nicolas <nicolasdemaria4@gmail.com>
2022-07-29 09:09:55 -0300

    Update modulosUtilesArrays.py

--- Unit05/modulosUtilesArrays.py
+++ Unit05/modulosUtilesArrays.py
@@ -5,3 +5,13 @@ for j in range(i+1, n):
     v = []
     if v[i] > v[j]:
         v[i], v[j] = v[j], v[i]
+
+
+def selection_sort(v):
+    n = len(v)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if v[i] > v[j]:
+                v[i], v[j] = v[j], v[i]
+
+# TODO búsqueda secuencial
