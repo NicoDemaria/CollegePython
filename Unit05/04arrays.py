5c21a60406730c8fee6f68ce8deae8c3061abb9d
Nicolas <nicolasdemaria4@gmail.com>
2022-08-01 22:14:23 -0300

    Update 04arrays.py

--- Unit05/04arrays.py
+++ Unit05/04arrays.py
@@ -1,23 +1,20 @@
-'''Desarrollar un programa que permita cargar tres arreglos con n nombres de personas, sus edades y los sueldos que ganan. Luego de realizar la carga de todos los datos, mostrar los nombres de las personas mayores de 18 años que ganan menos de 10000 pesos, pero de 
+'''Desarrollar un programa que permita cargar tres arreglos con n nombres de personas, sus edades y los sueldos que ganan. Luego de realizar la carga de todos los datos, mostrar los nombres de las personas mayores de 18 años que ganan menos de 10000 pesos, pero de
 forma que el listado salga ordenado en forma alfabética.'''
 
 
 def main():
-    n = input('Ingrese la cantidad de modulos de el arreglo:')
+    n = int(input('Ingrese la cantidad de modulos de el arreglo:'))
     nombre = n * [0]
     edad = n * [0]
     sueldo = n * [0]
-    for i in range(len(n)):
-        a = input('Ingrese el nombre de la la persona:')
-        nombre.append(a)
-        b = input('Ingrese la edad  de la la persona:')
-        edad.append(b)
-        c = input('Ingrese el sueldo de la persona de la la persona:')
-        sueldo.append(c)
+    for i in range(n):
+        nombre[i] = input('Ingrese el nombre de la la persona:')
+        edad[i] = input('Ingrese la edad  de la la persona:')
+        sueldo[i] = input('Ingrese el sueldo de la persona de la la persona:')
     return nombre, edad, sueldo
 
 
 nombre, edad, sueldo = main()
 print(nombre)
-print(edad)
-print(sueldo)
+print(edad, 'Years')
+print(sueldo, '$')
