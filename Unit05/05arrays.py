ee92df911a10a41b197189590bb1bb75355c0723
Nicolas <nicolasdemaria4@gmail.com>
2022-08-02 22:08:32 -0300

    Update 05arrays.py

--- Unit05/05arrays.py
+++ Unit05/05arrays.py
@@ -7,36 +7,28 @@ Determinar el promedio de lluvias para un determinado trimestre, 2
 Determinar el mes más seco del año, 3
 Determinar los meses del año en los que llovió más que el promedios de lluvia de todo el año., 4
 '''
+# notas = []
+# for i in range(4):
+#     x = int(input('Ingrese nota: '))
+#     notas.append(x)
+# print('Notas:', notas)
+# suma = 0
+# for i in range(4):
+#     suma += notas[i]
+#     promedio = suma / 4
+# print('Promedio:', promedio)
 
 
-def promedioAnualluvias(lluvias):
-    for i in lluvias:
-        total += i
-        promedio = total // 12
-    return promedio
+def promedioAnual(lluviasanuales):
+    pass
 
 
 def main():
     lluvia = []
-    meses = 12
-
-    for i in range(meses):
-        print('Mes: ', i+1)
-        mensual = input('Ingrese la precipitacion promedio de el mes: ')
-        lluvia.append(mensual)
-
-    print('Determinar el promedio anual de lluvias, 1 ')
-    print('Determinar el promedio de lluvias para un determinado trimestre, 2')
-    print('Determinar el mes más seco del año, 3')
-    print('Determinar los meses del año en los que llovió más que el promedios de lluvia de todo el año., 4')
-    opccion = str(
-        input('Ingrese una opcion porfavor, con 0 se termina el programa'))
-    while opccion != 0:
-        if opccion == 1:
-            promedioAnualluvias = promedioAnualluvias()
-            pass
-
-    return lluvia, promedioAnualluvias
+    for i in range(12):
+        x = input('Ingrese la cantidad de precipitaciones: ')
+        lluvia.append(x)
+    input('Ingrese el codigo')
 
 
-lluvias, promedioAnualluvias = main()
+lluviasanuales = main()
