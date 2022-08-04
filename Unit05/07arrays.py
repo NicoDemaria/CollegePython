6ec3c93dc0a742e28ee88123d3dd6d1571d372ed
Nicolas <nicolasdemaria4@gmail.com>
2022-08-04 12:08:12 -0300

    Create 07arrays.py

---
+++ Unit05/07arrays.py
@@ -0,0 +1,5 @@
+# 2. Último y Primero
+# Desarrollar un programa que permita cargar por teclado un vector de n elementos y luego:
+
+# Informe cuántas veces se repite en el vector el último número ingresado
+# Genere un nuevo vector, conteniendo sólo los elementos menores al primer valor ingresado.
