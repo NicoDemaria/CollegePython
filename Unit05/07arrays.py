5e86b9e6e38e778969d75bd5346c1527d46833f3
Nicolas <nicolasdemaria4@gmail.com>
2022-08-08 11:56:02 -0300

    Terminado

--- Unit05/07arrays.py
+++ Unit05/07arrays.py
@@ -5,9 +5,46 @@
 # Genere un nuevo vector, conteniendo sólo los elementos menores al primer valor ingresado.
 
 
+def validartamanio():
+    n = int(input('Ingrese el tamanio de el vector: '))
+    while n <= 0:
+        n = int(input('El tamaño debe ser positivo. Ingrese otro: '))
+    return n
+
+
+def cargaDatos(n):
+    vector = []
+    for i in range(n):
+        dato = int(input('Ingrese el elemento: '))
+        vector.append(dato)
+    return vector
+
+
+def contadorUltimo(vector):
+    repecionesUlt = 0
+    for elemento in vector:
+        if elemento == vector[-1]:
+            repecionesUlt += 1
+    return repecionesUlt - 1
+
+
+def contadorPrimer(vector):
+    menores = []
+    primerValor = vector[0]
+    for elemento in vector:
+        if elemento < primerValor:
+            menores.append(elemento)
+    return menores
+
+
 def main():
-    n = int(input('Ingrese la cantidad de elementos de el vector: '))
-    vector = [0] * n
+    tamanio = validartamanio()
+    vector = cargaDatos(tamanio)
+    cantidadUltimo = contadorUltimo(vector)
+    menores = contadorPrimer(vector)
+    print('Se repite el ultimo numero ingresado: ',
+          cantidadUltimo,  'cantida de veces')
+    print('Los elementos menores al primer valor ingresado son los siguientes: ', menores)
 
 
 main()
