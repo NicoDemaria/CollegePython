e5f7151bacebb5729f9540c5081a3862e04d21c5
Nicolas <nicolasdemaria4@gmail.com>
2022-08-10 14:57:32 -0300

    Update 013arrays.py

--- Unit05/013arrays.py
+++ Unit05/013arrays.py
@@ -5,8 +5,53 @@ Como parte de un estudio climatológico, se desea un programa que permita obtene
 Se pide un programa que:
 
 Ingrese n muestras de temperatura, donde cada muestra contiene la temperatura registrada, la región donde se registró la misma (1-20), y el día del mes en el que se registró la temperatura
-Determinar el promedio general de temperatura
-Dada una región, mostrar las temperaturas de la misma, ordenadas por dia, de menor a mayor
-Dada una región, determinar si la temperatura de alguna muestra superó el valor x, ingresado por teclado.
-Determinar la cantidad de muestras por region (20 contadores)
+Determinar el promedio general de temperatura  1 
+Dada una región, mostrar las temperaturas de la misma, ordenadas por dia, de menor a mayor 2 
+Dada una región, determinar si la temperatura de alguna muestra superó el valor x, ingresado por teclado. 3 
+Determinar la cantidad de muestras por region (20 contadores) 4
 '''
+
+
+def cargaDatos():
+    n = int(input('Ingrese la cantidad de temperaturas a registrar: '))
+    temperaturas = [0] * n
+    regiones = [0] * n
+    dias = [0] * n
+    while n < 1:
+        n = int(input('Ingrese la cantidad de temperaturas a registrar: '))
+    for i in range(n):
+        temperatura = int(
+            input('Ingrese la temp:  '))
+        region = int(
+            input('Ingrese la region a registrar(1:20): '))
+        dia = int(input('Ingrese el dia de la temp: '))
+        temperaturas[i] = temperatura
+        regiones[i] = region
+        dias[i] = dia
+    return temperaturas, regiones, dias, n
+
+
+def promTemp(temperaturas, n):
+    total = 0
+    for i in range(n):
+        total += temperaturas[i]
+    return total/n
+
+
+def main():
+    temperaturas, regiones, dias, n = cargaDatos()
+    opc = 0
+    while opc != 5:
+        opc = int(input('Ingrese la opcion deseada(5 termina): '))
+        if opc == 1:
+            promedioTemp = promTemp(temperaturas, n)
+            print('El promedio de temperaturas es: ', promedioTemp)
+        elif opc == 2:
+            pass
+        elif opc == 5:
+            print('Hasta la proximaaaa')
+        else:
+            print('Ingrese un numero valido')
+
+
+main()
